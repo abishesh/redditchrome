17981d754ae62a3f7bd952f3e998603c9e505536
Abhishesh Joshi <i65270@d2hs.com>
2016-01-06 22:30:21 +0545

    added hyperlinks to urls

--- reddit.js
+++ reddit.js
@@ -31,6 +31,24 @@ function reddit(url) {
         .replace('\/.', '.')
         .replace('.json.json', '.json');
   }
+  
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
 
   fetch(processUrl(url))
     .then(function (res) { return res.json() })
@@ -55,7 +73,8 @@ function reddit(url) {
       //console.log();
       var res = JSON.stringify(results, null, 2);
       var content = document.getElementById("content");
-      content.innerHTML = res;
+     // console.log(linkify(res));
+      content.innerHTML = linkify(res);
       hljs.highlightBlock(content);
     })
     .catch(function (err) {
